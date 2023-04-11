ff2496e22efb945f8a1809f4fe85611bb09efb67
wuxianggujun <3344207732@qq.com>
2023-04-11 19:25:46 +0800

    前后端分离项目，这是空模板

--- src/main/java/com/wuxianggujun/example/studyprojectbackend/StudyProjectBackendApplication.java
+++ src/main/java/com/wuxianggujun/example/studyprojectbackend/StudyProjectBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudyProjectBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudyProjectBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectBackendApplication.class, args);
+    }
 
 }
